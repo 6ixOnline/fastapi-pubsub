cec1cd76e32b90918803d574415592cb90aa7777
Dhiogo Correa <dhiogo.correa@itera.com.br>
2023-08-27 08:37:16 -0300

    updated example

--- examples/pubsub_server_example.py
+++ examples/pubsub_server_example.py
@@ -1,9 +1,9 @@
 from fastapi import FastAPI, WebSocket
 
-from fastapi_pubsub.base import BaseWebSocket
+from fastapi_pubsub import BasePubSubWebSocket
 
 
-class SimpleWebsocket(BaseWebSocket):
+class SimplePubSubWebsocket(BasePubSubWebSocket):
     async def generate_object_to_send(self):
         connections = self.connection_manager.get(self.websocket_id)
         return {"num_connected_clients": len(connections)}
@@ -14,5 +14,5 @@ app = FastAPI()
 
 @app.websocket("/ws/connected_clients")
 async def online_players_endpoint(websocket: WebSocket):
-    online_players_ws = SimpleWebsocket(websocket, 'connected_clients')
-    await online_players_ws.run()
+    online_clients_ws = SimplePubSubWebsocket(websocket, 'connected_clients')
+    await online_clients_ws.run()
